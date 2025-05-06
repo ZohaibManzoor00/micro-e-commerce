518fd77eb715ded5aeb7d00f3297bef2e86b3746
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-05-06 15:39:02 -0400

    css variables and 6 themes setup

---
+++ components/theme-switcher.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useRef, useState } from "react";
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // New state to control dropdown visibility
+  const { setTheme, resolvedTheme, themes, theme } = useTheme();
+  const ref = useRef(null);
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted)
+    return (
+      <button
+        // size='small'
+        type="button"
+        className="text-destructive inline-flex w-fit min-w-[95px] items-center justify-between gap-3"
+        id="options-menu"
+        aria-expanded={isOpen}
+        onClick={() => {}}
+      >
+        <span className="ml-2">Theme</span>
+        {/* <FiSun /> */}
+      </button>
+    );
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div ref={ref} className="relative inline-block text-left">
+      <button
+        // size='small'
+        type="button"
+        className="text-destructive inline-flex w-full min-w-[95px] items-center justify-between gap-3"
+        id="options-menu"
+        aria-expanded={isOpen}
+        onClick={toggleDropdown}
+      >
+        <span className="ml-2">Theme</span>
+        {/* <FiSun /> */}
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-full origin-top-right rounded-md bg-dropdown shadow-lg">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            {themes.map((themeItem) => {
+              return (
+                <button
+                  key={themeItem}
+                  onClick={() => {
+                    setTheme(themeItem);
+                    setIsOpen(false);
+                  }}
+                  className={`block w-full px-4 py-2 text-left text-sm hover:bg-dropdownHover ${
+                    themeItem === theme
+                      ? "bg-selected text-primary hover:bg-selected"
+                      : "text-secondary"
+                  }`}
+                >
+                  {themeItem}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "./providers/theme-provider";
 import "./globals.css";
+import ThemeSwitch from "../../components/theme-switcher";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          themes={[
+            "light",
+            "dark",
+            "instagram",
+            "facebook",
+            "discord",
+            "netflix",
+            "twilight",
+            "reddit",
+          ]}
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
